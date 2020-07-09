b90ba5b148496c1d9fe3e04c2644700361639543
Elijah wines <elijahwines84@yahoo.com>
2020-07-09 14:32:44 +0000

    Automatically backed up by Learn

--- db/migrate/20200709143200_create_preferences.rb
+++ db/migrate/20200709143200_create_preferences.rb
@@ -3,6 +3,8 @@ class CreatePreferences < ActiveRecord::Migration[5.0]
     create_table :preferences do |t|
       t.boolean :allow_create_artists
       t.boolean :allow_create_songs
+
+      t.timestamps
     end
   end
 end
